31111bc146b87dc5db7303680ae4cbf89b9c4ad8
Gabo <gabriel.vasconez@epn.edu.ec>
2025-03-27 18:01:42 -0500

    Clase 05

--- WorkSpace_IntroToPrograAEIS/semana02/clase04/ejemplos_bucles.c
+++ WorkSpace_IntroToPrograAEIS/semana02/clase04/ejemplos_bucles.c
@@ -13,11 +13,14 @@ int main()
 
     // Bucle while
     int i = 0;
+
     while (i < 5)
     {
         printf("Iteracion %d\n", i);
         i++;
     }
+
+    i = 0;
 
     // Bucle do-while
     do
@@ -26,20 +29,34 @@ int main()
         i++;
     } while (i < 5);
 
+    printf("El valor de i es: %d\n", i);
+
     // Ejercicio practico:
     /*
     Haz que el programa pida al usuario un número y luego imprima la tabla de multiplicar de ese número (del 1 al 10),
     usando cada uno de los tres tipos de bucles (for, while y do-while).
     */
 
-    char input[10];
-    int numero;
+    char input[3];
+    int numero = 0;
 
-    printf("Ingresa un numero\n");
+    printf("Ingresa un numero entero: ");
+
     fgets(input, sizeof(input), stdin);
+
     numero = atoi(input);
 
-    printf("Su tabla de multiplicar es:\n");
+    printf("El numero que ingresaste es: %d\n", numero);
+
+    printf("La tabla de multiplicacion de ese numero es:\n");
+
+    printf("Alternativa 01:\n");
+    for (int i = 0; i < 10; i++)
+    {
+        printf("%d\n", numero * (i + 1));
+    }
+
+    printf("Alternativa 02:\n");
     for (int i = 1; i <= 10; i++)
     {
         printf("%d\n", numero * i);
