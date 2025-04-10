44b32368370787564be71804ff51657f77f2f921
Gabo <gabriel.vasconez@epn.edu.ec>
2025-04-10 16:46:22 -0500

    Actualizacion: Clase 11

--- WorkSpace_IntroToPrograAEIS/semana04/clase11/cadenasDeCaracteres/cadenasDeCaracteres.c
+++ WorkSpace_IntroToPrograAEIS/semana04/clase11/cadenasDeCaracteres/cadenasDeCaracteres.c
@@ -8,7 +8,19 @@ int main()
     printf("Nombre1: %s\n", nombre1);
     printf("Nombre2: %s\n", nombre2);
 
-    printf("%c", nombre2[4]);
+    /*
+    0 -> 'J'
+    1 -> 'u'
+    2 -> 'a'
+    3 -> 'n'
+    4 -> '\0'
+    */
+
+    printf("%c\n", nombre2[4]);
+
+    char nombre3[] = {80, 101, 112, 101, 0};
+
+    printf("El valor de nombre3 es: %s\n", nombre3);
 
     return 0;
 }
